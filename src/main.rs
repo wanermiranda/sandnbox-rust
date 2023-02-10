12e1a0d869998fbe2bc664ee82f0bd776f6935e4
Waner Miranda <wanermiranda@gmail.com>
2023-02-09 23:10:56 -0300

    Playing a little with onnx

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,36 @@
 mod utilities;
-
-use colored::Colorize; // needed for time
+use colored::Colorize;
+use scanf::scanf; // needed for time
 
 use models::xml_roberta;
+use onnxruntime::{environment, LoggingLevel};
+use terminal_menu::mut_menu;
 use utilities::tokens::{bench_tonizers, generate_random_tokens};
 
 mod models;
 use models::token_bert;
 
 fn main() {
+    use terminal_menu::{button, label, menu, run};
+    let menu = menu(vec![
+        label("--------------"),
+        label("There are some basic tasks on this sandbox."),
+        label("---------------"),
+        button("rust_bert"),
+        button("onnx"),
+        button("exit"),
+    ]);
+    run(&menu);
+    let mm = mut_menu(&menu);
+
+    match mm.selected_item_name() {
+        "rust_bert" => rust_bert_models(),
+        "onnx" => onnx_models(),
+        i => println!("Menu item {} not found.", i),
+    }
+}
+
+fn rust_bert_models() {
     let input = [
         "My name is Amélie. I live in Москва.",
         "Chongqing is a city in China.",
@@ -50,5 +72,40 @@ fn main() {
         for token in token_outputs {
             println!("{token:?}");
         }
+    }
+}
+
+fn onnx_models() {
+    let builder = environment::Environment::builder()
+        .with_name("onnx_metadata")
+        .with_log_level(LoggingLevel::Verbose);
+
+    let environment = builder.build().unwrap();
+
+    // provide path to .onnx model on disk
+    let path: String = String::from(
+        "/home/gorigan/projects/sandbox-rust/resources/roberta-sequence-classification-9.onnx",
+    );
+
+    let session = environment
+        .new_session_builder()
+        .unwrap()
+        .with_model_from_file(path)
+        .unwrap();
+
+    println!("Inputs:");
+    for (index, input) in session.inputs.iter().enumerate() {
+        println!(
+            "  {}:\n    name = {}\n    type = {:?}\n    dimensions = {:?}",
+            index, input.name, input.input_type, input.dimensions
+        )
+    }
+
+    println!("Outputs:");
+    for (index, output) in session.outputs.iter().enumerate() {
+        println!(
+            "  {}:\n    name = {}\n    type = {:?}\n    dimensions = {:?}",
+            index, output.name, output.output_type, output.dimensions
+        );
     }
 }
